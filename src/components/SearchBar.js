e74298f37722a107423ea7aef023f55d2f57b8ff
Jose Ramirez <jose@ualberta.ca>
2020-02-07 15:45:06 -0700

    Added more visual fidelity to the main search page

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,12 +4,16 @@ import { connect } from 'react-redux';
 import TokenInput from 'react-customize-token-input';
 // Be sure to include styles at some point, probably during your bootstraping
 import 'react-customize-token-input/dist/react-customize-token-input.css';
+import styles from 'styles/searchbar.module.css';
 
 import { executeSearch } from 'actions';
 
 // const red = '#fd5e25';
 const COLOR_ORANGE = '#fdbb1d';
+const EGG_WHITE = '#EAEAEA';
 // const green = '#84fd25';
+const COLOR_LIGHTGREEN = '#4BBF84';
+const COLOR_BLUE = '#00A2FF';
 
 type Props = {|
   text: string,
@@ -17,15 +21,13 @@ type Props = {|
 |};
 
 const BORDER_WIDTH = 1;
-const CORNER_RADIUS = 20;
+const CORNER_RADIUS = 13;
 
 const style = {
   outerSearchBox: {
-    border: `${BORDER_WIDTH}px solid black`,
-    borderTopLeftRadius: CORNER_RADIUS,
-    borderBottomRightRadius: CORNER_RADIUS,
-    paddingLeft: CORNER_RADIUS,
-    paddingBottom: 0,
+    border: `${BORDER_WIDTH}px solid ${EGG_WHITE}`,
+    borderRadius: `${CORNER_RADIUS}px`,
+    padding: CORNER_RADIUS - BORDER_WIDTH,
     boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.1)',
     height: 'auto',
     display: 'flex',
@@ -41,11 +43,13 @@ const style = {
     height: 40,
   },
   searchButton: {
-    height: 'auto',
     flex: `0 1 ${CORNER_RADIUS + 50}`,
     alignSelf: 'stretch',
-    backgroundColor: COLOR_ORANGE,
-    borderBottomRightRadius: CORNER_RADIUS - BORDER_WIDTH,
+    color: 'grey',
+    borderRadius: `${CORNER_RADIUS}px`,
+    margin: '0 auto',
+    marginTop: `${CORNER_RADIUS}px`,
+    padding: `${CORNER_RADIUS}px`,
   },
 };
 
@@ -53,12 +57,16 @@ class SearchBar extends React.PureComponent<Props> {
   render() {
     // const { text } = this.props;
     return (
-      <div style={style.outerSearchBox}>
-        <TokenInput style={style.editBox} autoFocus="true" />
-        <button type="button" className="searchButton"
-          style={style.searchButton} onClick={this.props.executeSearch}>
-            Search
-        </button>
+      <div className={styles.searchContainer}>
+        <div style={style.outerSearchBox}>
+          <TokenInput style={style.editBox} autoFocus="true" />
+        </div>
+        <div className={styles.buttonContainer}>
+          <button type="button" className={styles.searchButtonAttitude}
+            style={style.searchButton} onClick={this.props.executeSearch}>
+              Search
+          </button>
+        </div>
       </div>
     );
   }
