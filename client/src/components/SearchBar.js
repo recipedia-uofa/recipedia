ba6623f3d9575529d0861109fa244f1fbf6b6974
Jose Ramirez <jose@ualberta.ca>
2020-02-18 15:00:35 -0700

    Created the first draft for token style renders

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -6,15 +6,10 @@ import TokenInput from 'react-customize-token-input';
 // Be sure to include styles at some point, probably during your bootstraping
 import 'react-customize-token-input/dist/react-customize-token-input.css';
 import styles from 'styles/searchbar.module.css';
+import * as colours from 'constants/colours';
 
 import { executeSearch } from 'actions';
-
-// const red = '#fd5e25';
-const COLOR_ORANGE = '#fdbb1d';
-const EGG_WHITE = '#EAEAEA';
-// const green = '#84fd25';
-const COLOR_LIGHTGREEN = '#4BBF84';
-const COLOR_BLUE = '#00A2FF';
+import type { PartialSearchToken, FullSearchToken, SearchToken } from 'types/tokens';
 
 type Props = {|
   text: string,
@@ -22,11 +17,12 @@ type Props = {|
 |};
 
 const BORDER_WIDTH = 1;
-const CORNER_RADIUS = 13;
+const CORNER_RADIUS = 15;
+const KEYWORD_MARGIN = 5;
 
 const style = {
   outerSearchBox: {
-    border: `${BORDER_WIDTH}px solid ${EGG_WHITE}`,
+    border: `${BORDER_WIDTH}px solid ${colours.LIGHT_BACKGROUND_DEFAULT_COLOUR}`,
     borderRadius: `${CORNER_RADIUS}px`,
     padding: CORNER_RADIUS - BORDER_WIDTH,
     boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.1)',
@@ -54,13 +50,113 @@ const style = {
   },
 };
 
+// NOTE: For the full token container, need to add a margin-right of 5px
+const searchTokenStyle = {
+  partialTokenContainer: {
+    borderTopLeftRadius: `${CORNER_RADIUS}px`,
+    borderBottomLeftRadius: `${CORNER_RADIUS}px`,
+    display: 'flex',
+    flexDirection: 'row',
+    width: 'max-content',
+    margin: `${KEYWORD_MARGIN*2}px`,
+    border: `3px solid ${colours.SHADE_LVL1}`,
+  },
+  partialTokenName: {
+    fontWeight: '600',
+    color: 'white',
+    alignSelf: 'center',
+    padding: '8px',
+  },
+  fullTokenContainer: {
+    borderadius: `${CORNER_RADIUS - 10}px`,
+    display: 'flex',
+    flexDirection: 'row',
+    width: 'max-content',
+    margin: `${KEYWORD_MARGIN*2}px`,
+    overflow: 'hidden',
+  },
+  fullTokenName: {
+    marginRight: `${KEYWORD_MARGIN}px`,
+    borderTopLeftRadius: `${CORNER_RADIUS-5}px`,
+    borderBottomLeftRadius: `${CORNER_RADIUS-5}px`,
+    fontWeight: '600',
+    backgroundColor: `${colours.SHADE_LVL2}`,
+    color: 'white',
+    alignSelf: 'center',
+    padding: '11px',
+  },
+  tokenContent: {
+    margin: '9px',
+    marginLeft: '0',
+    padding: '2px',
+    borderTopLeftRadius: `${CORNER_RADIUS-5}px`,
+    borderBottomLeftRadius: `${CORNER_RADIUS-5}px`,
+    color: 'white',
+  },
+};
+
+const getTokenColour = (token: SearchToken): string => {
+  switch(token.keyword) {
+    case 'not':
+      return colours.NOT_KEYWORD_COLOUR;
+    case 'key':
+      return colours.KEY_KEYWORD_COLOUR;
+    case 'diet':
+      return colours.DIET_KEYWORD_COLOUR;
+    default:
+      return colours.INGREDIENT_COLOUR;
+  }
+};
+
+const renderPartialToken = (token: PartialSearchToken) => {
+  const tokenColour = getTokenColour(token);
+  const keyword = token.keyword.toUpperCase();
+  return (
+    <div style={{
+        ...searchTokenStyle.partialTokenContainer,
+        backgroundColor: tokenColour,
+      }}>
+      <div style={searchTokenStyle.partialTokenName}>{keyword}</div>
+    </div>
+  );
+};
+
+const renderFullToken = (token : FullSearchToken) => {
+  const tokenColour = getTokenColour(token);
+  const keyword = token.keyword.toUpperCase();
+  const content = (token.ingredient || token.diet).toUpperCase();
+  return (
+    <div style={{
+      ...searchTokenStyle.fullTokenContainer,
+      backgroundColor: tokenColour,
+    }}>
+      <div style={searchTokenStyle.fullTokenName}>{keyword}</div>
+      <div style={searchTokenStyle.tokenContent}>{content}</div>
+    </div>
+  );
+};
+
+
+const isPartial = (token) => {
+  // Check the type of the token from the flow props
+  const keys = Object.keys(token);
+  return keys.length === 1 && keys.includes('keyword');
+};
+
+const renderToken = (token: SearchToken) => {
+  return isPartial(token) 
+    ? renderPartialToken(token)
+    : renderFullToken(token);
+};
+
 class SearchBar extends React.PureComponent<Props> {
   render() {
     // const { text } = this.props;
     return (
       <div className={styles.searchContainer}>
         <div style={style.outerSearchBox}>
-          <TokenInput style={style.editBox} autoFocus={true} />
+          <TokenInput style={style.editBox} autoFocus
+            tokenLabelRender={renderToken}/>
         </div>
         <div className={styles.buttonContainer}>
           <button type="button" className={styles.searchButtonAttitude}
---
+++ client/src/constants/colours.js
@@ -0,0 +1,29 @@
+// @flow
+// Site colour constants
+
+export const KEY_KEYWORD_COLOUR = '#D2A734';
+
+export const NOT_KEYWORD_COLOUR = '#CD4632';
+
+export const DIET_KEYWORD_COLOUR = '#89609E';
+
+export const INGREDIENT_COLOUR = '#4BBF84';
+
+export const PRIMARY_COLOUR = '#4BBF84';
+
+export const LIGHT_BACKGROUND_DEFAULT_COLOUR = '#EAEAEA';
+
+export const DARK_BACKGROUND_DEFAULT_COLOUR = '#3C996A';
+
+export const LIGHT_FONT_COLOUR = '#EAEAEA';
+
+export const DARK_FONT_COLOUR = 'grey';
+
+// These shade levels MUST be a part of div elements above a background layer that already exists
+export const SHADE_LVL1 = 'rgba(0, 0, 0, 0.1)';
+
+export const SHADE_LVL2 = 'rgba(0, 0, 0, 0.2)';
+
+export const SHADE_LVL3 = 'rgba(0, 0, 0, 0.5)';
+
+// For additional colours, including that of logos, possibly use this tool: https://lokeshdhakar.com/projects/color-thief/
