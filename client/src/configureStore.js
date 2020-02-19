dab743122457365189b0d77698be50d55d7fbb58
James Hryniw <jhryniw@gmail.com>
2020-02-19 15:17:01 -0700

    filter logger (#29)

--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -1,8 +1,22 @@
 // @flow
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import rootReducer from './reducers/rootReducer';
+import { CHANGE_SEARCH_TEXT } from 'constants/actionTypes';
+
+import type { Action } from 'actions';
+import type { GetState } from 'types/states';
+
+// Filter specific action types from logger
+const filteredActionTypes = [
+    CHANGE_SEARCH_TEXT,
+];
+
+const logger = createLogger({
+    predicate: (getState: GetState, action: Action) =>
+        !filteredActionTypes.includes(action.type),
+});
 
 export default function configureStore(initialState:Object = {}) {
   return createStore(
