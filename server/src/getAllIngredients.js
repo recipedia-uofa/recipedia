4d0a195548e9972b3fe43a326af7a5073d483d38
jhryniw <jhryniw@gmail.com>
2020-02-28 21:59:02 -0700

    replace ingredient query to work with dgraph-js

---
+++ server/src/getAllIngredients.js
@@ -0,0 +1,25 @@
+// @flow
+import * as R from 'ramda';
+import query from './query';
+import { extractResult } from './dgraph-utils';
+
+type IngredientResult = {
+  xid: string,
+};
+
+const extractIngredients: Object => Array<IngredientResult>
+  = extractResult('allIngredients');
+
+const getIngredientName = (i: IngredientResult) => i.xid;
+
+const getAllIngredients = async (): Promise<Array<string>> => {
+  const result = await query(`{
+    allIngredients(func: eq(<dgraph.type>, "Ingredient")) {
+       xid
+    }
+  }`);
+
+  return extractIngredients(result).map(getIngredientName).sort();
+};
+
+export default getAllIngredients;
---
+++ server/src/query.js
@@ -0,0 +1,16 @@
+// @flow
+import dgraph from 'dgraph-js';
+import grpc from 'grpc';
+
+const clientStub = new dgraph.DgraphClientStub(
+  "localhost:9080",
+  grpc.credentials.createInsecure()
+);
+const dgraphClient = new dgraph.DgraphClient(clientStub);
+
+const runQuery = async (query: string) => {
+  const res = await dgraphClient.newTxn().query(query);
+  return res.getJson();
+};
+
+export default runQuery;
