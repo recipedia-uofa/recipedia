b19b5cf538708cdce084899285c0f385d701a57b
joseram97 <jose@ualberta.ca>
2020-03-02 16:22:08 -0700

    Fix recipecard. Now includes score. #20 (#56)
    * Implemented the nutrition score visual and modified recipe card styling
    
    * Refactored the RecipeCard component
    
    * Ensured recipe card renders properly

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -13,227 +13,229 @@ import proteinImg from "assets/icons/protein.svg";
 import sugarImg from "assets/icons/Sugar_Cube.svg";
 import servingsizeImg from "assets/icons/Fork_1.svg";
 
+import type { Node } from "react";
 import type { Recipe } from "models/recipe";
+import type { Ingredient } from "models/ingredient";
 
 type Props = {
   recipe: Recipe
 };
+
+type NutritionInfoProps = {
+  className: string,
+  details: number,
+  icon: string,
+  title: Node | string,
+  tag: string,
+  isLargeTitle: boolean,
+};
+
+class NutritionInfoCard extends React.PureComponent<NutritionInfoProps> {
+  render() {
+    const { className, details, icon, title, tag, isLargeTitle } = this.props;
+    return (
+      <div
+        className={classNames(
+          styles.SecondaryRecipeCard,
+          className
+        )}
+      >
+        <div className={styles.RecipeCardOffset}>
+          <div className={styles.NutritionDescriptionItem}>
+            <img
+              className={styles.NutritionDescriptionItemImage}
+              src={icon}
+              alt={title}
+            />
+            <div
+              className={isLargeTitle ? classNames(styles.LargeWordFont, styles.DietTitle) : styles.DietTitle}
+            >
+              {title}
+            </div>
+            <div
+              className={classNames(
+                styles.Center,
+                styles.NutritionDescriptionItem
+              )}
+            >
+              <div className={styles.Bold}>{details}</div>
+              <div className={isLargeTitle ? styles.SmallFont : styles.Bold}>{tag}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class SecondaryRecipeCards extends React.PureComponent<Props> {
+  render() {
+    const { recipe } = this.props;
+    return (
+      <div className={styles.SecondaryRecipeCardContainer}>
+        <NutritionInfoCard 
+          details={recipe.nutritionalInfo.calories}
+          icon={calorieImg}
+          title="Calories"
+          tag="kcal"
+          className={styles.CaloriesColor}
+          isLargeTitle={true}
+        />
+        <NutritionInfoCard 
+          details={recipe.nutritionalInfo.fat}
+          icon={fatImg}
+          title="Fat"
+          tag="g"
+          className={styles.FatColor}
+          isLargeTitle={false}
+        />
+        <NutritionInfoCard 
+          details={recipe.nutritionalInfo.carbs}
+          icon={carbsImg}
+          title="Carbs"
+          tag="g"
+          className={styles.CarbsColor}
+          isLargeTitle={false}
+        />
+        <NutritionInfoCard 
+          details={recipe.nutritionalInfo.protein}
+          icon={proteinImg}
+          title="Protein"
+          tag="g"
+          className={styles.ProteinColor}
+          isLargeTitle={false}
+        />
+        <NutritionInfoCard 
+          details={recipe.nutritionalInfo.sugar}
+          icon={sugarImg}
+          title="Sugar"
+          tag="g"
+          className={styles.SugarColor}
+          isLargeTitle={false}
+        />
+        <NutritionInfoCard 
+          details={recipe.servingSize}
+          icon={servingsizeImg}
+          title={<span>Serving<br></br>Size</span>}
+          tag="people"
+          className={styles.ServingSize}
+          isLargeTitle={true}
+        />
+      </div>
+    );
+  }
+}
+
+type IngredientTokenProps = {
+  ingredient: Ingredient,
+  isMatched: boolean,
+};
+
+class RecipeIngredientToken extends React.PureComponent<IngredientTokenProps> {
+  render() {
+    const { ingredient, isMatched } = this.props;
+    return (
+      <div key={ingredient} className={isMatched ?  styles.RecipeCardIngredientItem : classNames(styles.RecipeCardIngredientItem, styles.NotMatchedIngredient)}>
+        {ingredient}
+      </div>
+    );
+  }
+}
+
+type ScoreProps = {
+  recipeScore: number,
+}
+
+class RecipeScore extends React.PureComponent<ScoreProps> {
+  render() {
+    const { recipeScore } = this.props;
+    return (
+      <div className={styles.ScoreContainer}>
+        <div className={styles.ScoreCircle}>
+          <div className={styles.ScoreNumber} alt="0%">
+            {recipeScore}%
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+type LogoProps = {
+  logoImg: string,
+  logoAlt: string
+};
+
+class RecipeLogo extends React.PureComponent<LogoProps> {
+  render() {
+    const { logoImg, logoAlt } = this.props;
+    return (
+      <div className={styles.LogoContainer}>
+        <div className={styles.RecipeCardSiteLogo}>
+          <img
+            className={styles.RecipeCardSiteLogoImage}
+            src={logoImg}
+            alt={logoAlt}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+type IngredientBoxProps = {
+  matchedIngredients: Array<Ingredient>,
+  notMatchedIngredients: Array<Ingredient>,
+}
+
+class RecipeIngredientBox extends React.PureComponent<IngredientBoxProps> {
+  render() {
+    const { matchedIngredients, notMatchedIngredients } = this.props;
+    return (
+      <div className={styles.RecipeCardIngredientBox}>
+        {matchedIngredients.map(i => (
+          <RecipeIngredientToken ingredient={i} isMatched={true}/>
+        ))}
+        {notMatchedIngredients.map(i => (
+          <RecipeIngredientToken ingredient={i} isMatched={false}/>
+        ))}
+      </div>
+    );
+  }
+}
+
+class PrimaryRecipeCard extends React.PureComponent<Props> {
+  render() {
+    const { recipe } = this.props;
+    return (
+      <div className={styles.RecipeCardContainer}>
+        <div className={styles.RecipeCardImageContainer}>
+          <img
+            className={styles.RecipeCardImage}
+            src={eggPhoto}
+            alt={recipe.title}
+          />
+        </div>
+        <div className={styles.RecipeCardTitle}>{recipe.title}</div>
+        <div className={styles.RecipeCardDescription}>
+          <div className={styles.RecipeCardIngredientContainer}>
+            <RecipeIngredientBox matchedIngredients={recipe.ingredientsMatched} notMatchedIngredients={recipe.ingredientsNotMatched}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 class RecipeCard extends React.PureComponent<Props> {
   render() {
     const { recipe } = this.props;
     return (
       <div className={styles.RecipeCards}>
-        <div className={styles.LogoContainer}>
-          <div className={styles.RecipeCardSiteLogo}>
-            <img
-              className={styles.RecipeCardSiteLogoImage}
-              src={allRecipesLogo}
-              alt="AllRecipes.com"
-            />
-          </div>
-        </div>
-        <div className={styles.RecipeCardContainer}>
-          <div className={styles.RecipeCardImageContainer}>
-            <img
-              className={styles.RecipeCardImage}
-              src={eggPhoto}
-              alt={recipe.title}
-            />
-          </div>
-          <div className={styles.RecipeCardTitle}>{recipe.title}</div>
-          <div className={styles.RecipeCardDescription}>
-            <div className={styles.RecipeCardIngredientContainer}>
-              <div className={styles.RecipeCardIngredientBox}>
-                {recipe.ingredientsMatched.map(i => (
-                  <div key={i} className={styles.RecipeCardIngredientItem}>
-                    {i}
-                  </div>
-                ))}
-                {recipe.ingredientsNotMatched.map(i => (
-                  <div
-                    key={i}
-                    className={classNames(
-                      styles.RecipeCardIngredientItem,
-                      styles.NotMatchedIngredient
-                    )}
-                  >
-                    {i}
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={styles.SecondaryRecipeCardContainer}>
-          <div
-            className={classNames(
-              styles.SecondaryRecipeCard,
-              styles.CaloriesColor
-            )}
-          >
-            <div className={styles.RecipeCardOffset}>
-              <div className={styles.NutritionDescriptionItem}>
-                <img
-                  className={styles.NutritionDescriptionItemImage}
-                  src={calorieImg}
-                  alt="Calories"
-                />
-                <div
-                  className={classNames(styles.LargeWordFont, styles.DietTitle)}
-                >
-                  Calories
-                </div>
-                <div
-                  className={classNames(
-                    styles.Center,
-                    styles.NutritionDescriptionItem
-                  )}
-                >
-                  <div className={styles.Bold}>
-                    {recipe.nutritionalInfo.calories}
-                  </div>
-                  <div className={styles.SmallFont}>kcal</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div
-            className={classNames(styles.SecondaryRecipeCard, styles.FatColor)}
-          >
-            <div className={styles.RecipeCardOffset}>
-              <div className={styles.NutritionDescriptionItem}>
-                <img
-                  className={styles.NutritionDescriptionItemImage}
-                  src={fatImg}
-                  alt="Fat"
-                />
-                <div className={styles.DietTitle}>Fat</div>
-                <div
-                  className={classNames(
-                    styles.Center,
-                    styles.NutritionDescriptionItem
-                  )}
-                >
-                  <div className={styles.Bold}>
-                    {recipe.nutritionalInfo.fat}g
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div
-            className={classNames(
-              styles.SecondaryRecipeCard,
-              styles.CarbsColor
-            )}
-          >
-            <div className={styles.RecipeCardOffset}>
-              <div className={styles.NutritionDescriptionItem}>
-                <img
-                  className={styles.NutritionDescriptionItemImage}
-                  src={carbsImg}
-                  alt="Carbs"
-                />
-                <div className={styles.DietTitle}>Carbs</div>
-                <div
-                  className={classNames(
-                    styles.Center,
-                    styles.NutritionDescriptionItem
-                  )}
-                >
-                  <div className={styles.Bold}>
-                    {recipe.nutritionalInfo.carbs}g
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div
-            className={classNames(
-              styles.SecondaryRecipeCard,
-              styles.ProteinColor
-            )}
-          >
-            <div className={styles.RecipeCardOffset}>
-              <div className={styles.NutritionDescriptionItem}>
-                <img
-                  className={styles.NutritionDescriptionItemImage}
-                  src={proteinImg}
-                  alt="Protein"
-                />
-                <div className={styles.DietTitle}>Protein</div>
-                <div
-                  className={classNames(
-                    styles.Center,
-                    styles.NutritionDescriptionItem
-                  )}
-                >
-                  <div className={styles.Bold}>
-                    {recipe.nutritionalInfo.protein}g
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div
-            className={classNames(
-              styles.SecondaryRecipeCard,
-              styles.SugarColor
-            )}
-          >
-            <div className={styles.RecipeCardOffset}>
-              <div className={styles.NutritionDescriptionItem}>
-                <img
-                  className={styles.NutritionDescriptionItemImage}
-                  src={sugarImg}
-                  alt="Sugar"
-                />
-                <div className={styles.DietTitle}>Sugar</div>
-                <div
-                  className={classNames(
-                    styles.Center,
-                    styles.NutritionDescriptionItem
-                  )}
-                >
-                  <div className={styles.Bold}>
-                    {recipe.nutritionalInfo.sugar}g
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div
-            className={classNames(
-              styles.SecondaryRecipeCard,
-              styles.ServingSize
-            )}
-          >
-            <div className={styles.RecipeCardOffset}>
-              <div className={styles.NutritionDescriptionItem}>
-                <img
-                  className={styles.NutritionDescriptionItemImage}
-                  src={servingsizeImg}
-                  alt="ServingSize"
-                />
-                <div
-                  className={classNames(styles.LargeWordFont, styles.DietTitle)}
-                >
-                  Serving<br></br>Size
-                </div>
-                <div
-                  className={classNames(
-                    styles.Center,
-                    styles.NutritionDescriptionItem
-                  )}
-                >
-                  <div className={styles.Bold}>{recipe.servingSize}</div>
-                  <div className={styles.SmallFont}>people</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <RecipeLogo logoImg={allRecipesLogo} logoAlt="A|R"/>
+        <PrimaryRecipeCard recipe={recipe}/>
+        <RecipeScore recipeScore={recipe.nutritionScore}/>
+        <SecondaryRecipeCards recipe={recipe}/>
       </div>
     );
   }
