8fd03f496870b6a7e3784efe2084a0faa77fe918
lemonpopcorn <43223386+lemonpopcorn@users.noreply.github.com>
2020-03-29 10:55:03 -0600

    Add Category type and function to get blacklisted categories for diets (#143)
    Co-authored-by: lemonpopcorn <lemonpopcorn>

--- server/src/models/diets.js
+++ server/src/models/diets.js
@@ -1,4 +1,7 @@
 // @flow
+import type { Category } from "models/category";
+import categories from "models/category";
+
 const diets = {
   VEGETARIAN: "VEGETARIAN",
   VEGAN: "VEGAN",
@@ -13,6 +16,19 @@ export const isValidDiet = (str: string): boolean => {
 
 export const toDiet = (str: string): Diet => {
   return diets[str.toUpperCase()].toLowerCase();
+};
+
+export const getBlacklistedCategories = (diet: Diet): Array<Category> => {
+  switch (diet) {
+    case diets.VEGETARIAN:
+      return [categories.MEATS, categories.FISH];
+    case diets.VEGAN:
+      return [categories.MEATS, categories.FISH, categories.DAIRY];
+    case diets.GLUTEN_FREE:
+      return [categories.BAKING_AND_GRAINS];
+    default:
+      return [];
+  }
 };
 
 export default diets;
