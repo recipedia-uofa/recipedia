c8ae77655611276390b21218eec12582314fb731
lemonpopcorn <lemonpopcorn>
2020-04-03 15:55:44 -0600

    Address comments

--- server/src/models/recipe.js
+++ server/src/models/recipe.js
@@ -13,6 +13,7 @@ export type Recipe = {
   url: string, // unique
   title: string,
   rating: number, // 1-5
+  ratingScore: number,
   ingredientsMatched: Array<Ingredient>,
   ingredientsNotMatched: Array<Ingredient>,
   nutritionalInfo: NutritionalInfo,
